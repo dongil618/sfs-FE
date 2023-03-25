c7e44dda6329cc19c0f23e132ead0815e40261a1
dongil618 <rlaehddlf618@gmail.com>
2023-03-25 10:37:07 +0900

    chore : 각 메뉴별 페이지 생성
    - 키워드 관리, 이미지 조절, GIF 생성, 코디 관리, 거래처 관리 페이지 생성
    - 해당 페이지 라우팅 설정 완료

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ import Home from './page/Home';
 import Product from './page/Product';
 import GetProducts from './page/GetProducts';
 import SaveProducts from './page/SaveProducts';
+import Keyword from './page/Keyword';
+import Coordie from './page/Coordie';
+import Gif from './page/Gif';
+import Image from './page/Image';
+import Wholesale from './page/Wholesale';
 
 function App() {
   const menu_list = [
     { id: 1, title: "상품등록", link: "/product" },
+    { id: 2, title: "키워드 관리", link: "/keyword" },
+    { id: 3, title: "이미지 조절", link: "/image" },
+    { id: 4, title: "GIF 생성", link: "/gif" },
+    { id: 5, title: "코디 관리", link: "/coordie" },
+    { id: 6, title: "거래처 관리", link: "/wholesale" },
   ];
 
   return (
@@ -22,6 +32,11 @@ function App() {
             <Route path='/product' element={<Product />}></Route>
             <Route path='/products' element={<GetProducts />}></Route>
             <Route path='/products/save' element={<SaveProducts />}></Route>
+            <Route path='/keyword' element={<Keyword />}></Route>
+            <Route path='/image' element={<Image />}></Route>
+            <Route path='/gif' element={<Gif />}></Route>
+            <Route path='/coordie' element={<Coordie />}></Route>
+            <Route path='/wholesale' element={<Wholesale />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
